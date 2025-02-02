6f417adef047d92d369610e5c9df3354047f6ff2
Ben Cross <ssorc3@gmail.com>
2025-02-02 16:29:24 +0000

    Add a readme

--- internal/generate/linkId.go
+++ internal/generate/linkId.go
@@ -15,10 +15,13 @@ const (
 
 var src = rand.NewSource(time.Now().UnixNano())
 
+// NewLinkId generates a 7 letter random string
+//
+// Implementation from https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go#31832326
 func NewLinkId() string {
     n := 7
 	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+    // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
 			cache, remain = src.Int63(), letterIdxMax
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Verkürzen
+Rest API for URL shortening I made as a learning execise.
+
+## Technologies
+This API uses
+- [gin](https://github.com/gin-gonic/gin) as the router
+- [cassandra](https://cassandra.apache.org/_/index.html) as the database
+
